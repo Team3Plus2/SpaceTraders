2a5498f090b9d07746fae95786d672b0eaff87aa
Gaby <Gaby@Gaby-PC>
2014-09-15 12:53:45 -0400

    Character Config Screen now works how we want it to

--- SpaceTrader/src/spacetrader/CharacterConfigurationScreenController.java
+++ SpaceTrader/src/spacetrader/CharacterConfigurationScreenController.java
@@ -17,6 +17,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -44,7 +45,10 @@ public class CharacterConfigurationScreenController implements Initializable {
     private Label pilotLevelLabel, fighterLevelLabel, traderLevelLabel, engineerLevelLabel;
     
     @FXML
-    private Label progressBarLabel;
+    private Label skillPointsAvailableLabel;
+    
+    @FXML
+    private TextField name;
     
     /**
      * Initializes the controller class.
@@ -88,28 +92,50 @@ public class CharacterConfigurationScreenController implements Initializable {
     
     @FXML
     private void handleBeginAction(ActionEvent event) {
-        Player newPlayer = new Player("name", (int) pilotLevelSlider.getValue(), (int) fighterLevelSlider.getValue(), (int) traderLevelSlider.getValue(), (int) engineerLevelSlider.getValue(), 0);
+        
+        Player newPlayer = new Player(name.getText(), (int) pilotLevelSlider.getValue(), (int) fighterLevelSlider.getValue(), (int) traderLevelSlider.getValue(), (int) engineerLevelSlider.getValue(), 0);
         SpaceTrader.getInstance().goToGame();
     }
     
     @FXML
     private void handleProgressBarUpdate(ActionEvent event) {
-        double pilotSliderValue = pilotLevelSlider.getValue();
-        double fighterSliderValue = fighterLevelSlider.getValue();
-        double tradeSliderValue = traderLevelSlider.getValue();
-        double engineerSliderValue = engineerLevelSlider.getValue();
-        if(skillPointsAvailable.getProgress() > 0) {
-            pilotLevelLabel.setText(pilotSliderValue + "");
-            fighterLevelLabel.setText(fighterSliderValue + "");
-            traderLevelLabel.setText(tradeSliderValue + "");
-            engineerLevelLabel.setText(engineerSliderValue + "");
-            skillPointsAvailable.setProgress(1 - (pilotSliderValue/15.0 + fighterSliderValue/15.0 + tradeSliderValue/15.0 + engineerSliderValue/15.0));
-        } else if (pilotSliderValue <  pilotLevelSlider.getValue() || fighterSliderValue < fighterLevelSlider.getValue() || tradeSliderValue < traderLevelSlider.getValue() || engineerSliderValue < engineerLevelSlider.getValue()) {
-            pilotLevelSlider.adjustValue(pilotSliderValue);
-            fighterLevelSlider.adjustValue(fighterSliderValue);
-            traderLevelSlider.adjustValue(tradeSliderValue);
-            engineerLevelSlider.adjustValue(engineerSliderValue);
+        double pilotSliderValue = (double) pilotLevelSlider.getValue();
+        double fighterSliderValue = (double) fighterLevelSlider.getValue();
+        double tradeSliderValue = (double) traderLevelSlider.getValue();
+        double engineerSliderValue = (double) engineerLevelSlider.getValue();
+//        if(skillPointsAvailable.getProgress() >= 0) {
+        pilotLevelLabel.setText((int)pilotSliderValue + "");
+        fighterLevelLabel.setText((int)fighterSliderValue + "");
+        traderLevelLabel.setText((int)tradeSliderValue + "");
+        engineerLevelLabel.setText((int)engineerSliderValue + "");
+        if (15 - ((int)fighterSliderValue - (int)tradeSliderValue - (int)engineerSliderValue) >= 0) {
+            pilotLevelSlider.setMax(15 - (int)fighterSliderValue - (int)tradeSliderValue - (int)engineerSliderValue);
+        } else {
+            pilotLevelSlider.setMax(0);
         }
+        if (15 - ((int)pilotSliderValue - (int)tradeSliderValue - (int)engineerSliderValue) >= 0) {
+            fighterLevelSlider.setMax(15 - (int)pilotSliderValue - (int)tradeSliderValue - (int)engineerSliderValue);
+        } else {
+            fighterLevelSlider.setMax(0);
+        }
+        if (15 - ((int)fighterSliderValue - (int)pilotSliderValue - (int)engineerSliderValue) >= 0) {
+            traderLevelSlider.setMax(15 - (int)fighterSliderValue - (int)pilotSliderValue - (int)engineerSliderValue);
+        } else {
+            traderLevelSlider.setMax(0);
+        }
+        if (15 - ((int)fighterSliderValue - (int)tradeSliderValue - (int)pilotSliderValue) >= 0) {
+            engineerLevelSlider.setMax(15 - (int)fighterSliderValue - (int)tradeSliderValue - (int)pilotSliderValue);
+        } else {
+            engineerLevelSlider.setMax(0);
+        }
+        skillPointsAvailableLabel.setText((15 - (int)pilotSliderValue - (int)fighterSliderValue - (int)tradeSliderValue - (int)engineerSliderValue) + "/15");
+        skillPointsAvailable.setProgress(1 - (pilotSliderValue/15.0 + fighterSliderValue/15.0 + tradeSliderValue/15.0 + engineerSliderValue/15.0));
+//        if (pilotSliderValue <  pilotLevelSlider.getValue() || fighterSliderValue < fighterLevelSlider.getValue() || tradeSliderValue < traderLevelSlider.getValue() || engineerSliderValue < engineerLevelSlider.getValue()) {
+//            pilotLevelSlider.adjustValue(Math.floor(pilotSliderValue));
+//            fighterLevelSlider.adjustValue(Math.floor(fighterSliderValue));
+//            traderLevelSlider.adjustValue(Math.floor(tradeSliderValue));
+//            engineerLevelSlider.adjustValue(Math.floor(engineerSliderValue));
+//        }
     }
     
 }

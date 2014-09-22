e3e7fc461f298028d73a801235a9d21aad16469e
Careyjmac <careyjmac@hotmail.com>
2014-09-21 22:13:24 -0400

    Added TradeGood class

---
+++ SpaceTrader/src/spacetrader/economy/TradeGood.java
@@ -0,0 +1,22 @@
+package spacetrader.economy;
+
+import spacetrader.cosmos.system.TechLevel;
+import spacetrader.cosmos.system.Resources;
+
+/**
+ *
+ * @author Carey MacDonald
+ */
+public class TradeGood {
+    private int amount;
+    private String name;
+    private float basePrice, increasePerLevel, priceVariance, minRandPrice, maxRandPrice;
+    private TechLevel minLevelProduce, minLevelUse, levelProduceMost;
+    private Resources priceLowCondition, priceHighCondition;
+    
+    private float currentPrice;
+    
+    public void computeCurrentPrice(TechLevel planetLevel) {
+        
+    }
+}

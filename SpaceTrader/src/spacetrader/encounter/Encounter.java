7b5d47c30402e8448ec1579e091cf561f7df2155
ArrowMac <aaronmcanally@gatech.edu>
2014-09-30 14:43:48 -0400

    Added Encounters

---
+++ SpaceTrader/src/spacetrader/encounter/Encounter.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spacetrader.encounter;
+
+/**
+ * An interface for the trader, pirate, and police encounters
+ * 
+ * @author Aaron McAnally
+ */
+public interface Encounter {
+    
+}

fcf462dc9d05cba4f07d2b654ccc927abe2b12da
ArrowMac <aaronmcanally@gatech.edu>
2014-09-30 20:23:17 -0400

    Encounter Package

--- SpaceTrader/src/spacetrader/encounter/Pirate.java
+++ SpaceTrader/src/spacetrader/encounter/Pirate.java
@@ -5,12 +5,31 @@
  */
 package spacetrader.encounter;
 
+import spacetrader.economy.TradeGood;
+import spacetrader.main.SpaceTrader;
+import spacetrader.player.Player;
+import spacetrader.player.Ship;
+
 /**
  *
  * @author Aaron McAnally
  */
 public class Pirate implements Encounter {
-    public void handleEncounter() {
+    private Player pirate;
+    
+    public Pirate() {
+        int traderSkill = (int) (Math.random() * 10 + 1);
+        pirate = new Player("trader", 5, 5, traderSkill, 5, 5);
         
+        pirate.setShip(new Ship(Ship.ShipType.FIREFLY));
+        int cargoFilled = (int) (Math.random() * 20 + 1);
+        for (int i = 0; i < cargoFilled; i++) {
+            pirate.getShip().getCargo().addTradeGood(new TradeGood());
+        }
+    }
+    
+    @Override
+    public void handleEncounter() {
+        Player player = SpaceTrader.getInstance().getPlayer();
     }
 }
--- SpaceTrader/src/spacetrader/encounter/Trader.java
+++ SpaceTrader/src/spacetrader/encounter/Trader.java
@@ -5,12 +5,33 @@
  */
 package spacetrader.encounter;
 
+import spacetrader.economy.TradeGood;
+import spacetrader.main.SpaceTrader;
+import spacetrader.player.Player;
+import spacetrader.player.Ship;
+
 /**
- *
+ * Handles the trader encounter
+ * 
  * @author Aaron McAnally
  */
 public class Trader implements Encounter {
-    public void handleEncounter() {
+    
+    private Player trader;
+    
+    public Trader() {
+        int traderSkill = (int) (Math.random() * 10 + 1);
+        trader = new Player("trader", 5, 5, traderSkill, 5, 5);
         
+        trader.setShip(new Ship(Ship.ShipType.FIREFLY));
+        int cargoFilled = (int) (Math.random() * 20 + 1);
+        for (int i = 0; i < cargoFilled; i++) {
+            trader.getShip().getCargo().addTradeGood(new TradeGood());
+        }
+    }
+    
+    @Override
+    public void handleEncounter() {
+        Player player = SpaceTrader.getInstance().getPlayer();
     }
 }

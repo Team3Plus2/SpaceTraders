22f04ae5aa87c9fb1284660ee3c2a4a76ce3a655
Gaby <Gaby@Gaby-PC>
2014-11-03 21:57:11 -0500

    Javadocked and fixed checkstyle

--- SpaceTrader/src/spacetrader/view/CharacterConfigurationScreenController.java
+++ SpaceTrader/src/spacetrader/view/CharacterConfigurationScreenController.java
@@ -10,7 +10,6 @@ import spacetrader.main.SpaceTrader;
 import spacetrader.player.Player;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -21,33 +20,75 @@ import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 
 /**
- * FXML Controller class
+ * FXML Controller class.
  *
  * @author KartikKini
  */
 public class CharacterConfigurationScreenController implements Initializable {
 
+    /**
+     * Label for pilot skill.
+     */
     @FXML
     private Slider pilotLevelSlider;
     
+    /**
+     * Label for fighter skill.
+     */
     @FXML
     private Slider fighterLevelSlider;
     
+    /**
+     * Label for trader skill.
+     */
     @FXML
     private Slider traderLevelSlider;
     
+    /**
+     * Label for engineer skill.
+     */
     @FXML
     private Slider engineerLevelSlider;
     
+    /**
+     * Progress bar for skill points.
+     */
     @FXML
     private ProgressBar skillPointsAvailable;
     
+    /**
+     * Label for pilot skill.
+     */
     @FXML
-    private Label pilotLevelLabel, fighterLevelLabel, traderLevelLabel, engineerLevelLabel;
+    private Label pilotLevelLabel; 
     
+    /**
+     * Label for fighter skill.
+     */
+    @FXML
+    private Label fighterLevelLabel;
+    
+    /**
+     * Label for trader skill.
+     */
+    @FXML
+    private Label traderLevelLabel;
+    
+    /**
+     * Label for engineer skill.
+     */
+    @FXML
+    private Label engineerLevelLabel;
+    
+    /**
+     * Label for skill points.
+     */
     @FXML
     private Label skillPointsAvailableLabel;
     
+    /**
+     * Name of player.
+     */
     @FXML
     private TextField name;
     
@@ -57,24 +98,32 @@ public class CharacterConfigurationScreenController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         pilotLevelSlider.valueProperty().addListener((ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
-            handleProgressBarUpdate(new ActionEvent());
-        });
+                handleProgressBarUpdate(new ActionEvent());
+            });
         fighterLevelSlider.valueProperty().addListener((ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
-            handleProgressBarUpdate(new ActionEvent());
-        });
+                handleProgressBarUpdate(new ActionEvent());
+            });
         traderLevelSlider.valueProperty().addListener((ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
-            handleProgressBarUpdate(new ActionEvent());
-        });
+                handleProgressBarUpdate(new ActionEvent());
+            });
         engineerLevelSlider.valueProperty().addListener((ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
-            handleProgressBarUpdate(new ActionEvent());
-        });
+                handleProgressBarUpdate(new ActionEvent());
+            });
     }
     
+    /**
+     * If back button is pushed go back to welcome screen.
+     * @param event something happened
+     */
     @FXML
     private void handleBackAction(ActionEvent event) {
         SpaceTrader.getInstance().goToLoadGame();
     }
     
+    /**
+     * If begin is pushed go to the game screen.
+     * @param event something happened
+     */
     @FXML
     private void handleBeginAction(ActionEvent event) {
         if (!(name.getText().equals("")) && skillPointsAvailable.getProgress() == 0) {
@@ -89,6 +138,10 @@ public class CharacterConfigurationScreenController implements Initializable {
         }
     }
     
+    /**
+     * If skills change update the progress bar.
+     * @param event something happened
+     */
     @FXML
     private void handleProgressBarUpdate(ActionEvent event) {
         double pilotSliderValue = (double) pilotLevelSlider.getValue();
@@ -96,32 +149,32 @@ public class CharacterConfigurationScreenController implements Initializable {
         double tradeSliderValue = (double) traderLevelSlider.getValue();
         double engineerSliderValue = (double) engineerLevelSlider.getValue();
 //        if(skillPointsAvailable.getProgress() >= 0) {
-        pilotLevelLabel.setText((int)pilotSliderValue + "");
-        fighterLevelLabel.setText((int)fighterSliderValue + "");
-        traderLevelLabel.setText((int)tradeSliderValue + "");
-        engineerLevelLabel.setText((int)engineerSliderValue + "");
-        if (15 - ((int)fighterSliderValue - (int)tradeSliderValue - (int)engineerSliderValue) >= 0) {
-            pilotLevelSlider.setMax(15 - (int)fighterSliderValue - (int)tradeSliderValue - (int)engineerSliderValue);
+        pilotLevelLabel.setText((int) pilotSliderValue + "");
+        fighterLevelLabel.setText((int) fighterSliderValue + "");
+        traderLevelLabel.setText((int) tradeSliderValue + "");
+        engineerLevelLabel.setText((int) engineerSliderValue + "");
+        if (15 - ((int) fighterSliderValue - (int) tradeSliderValue - (int) engineerSliderValue) >= 0) {
+            pilotLevelSlider.setMax(15 - (int) fighterSliderValue - (int) tradeSliderValue - (int) engineerSliderValue);
         } else {
             pilotLevelSlider.setMax(0);
         }
-        if (15 - ((int)pilotSliderValue - (int)tradeSliderValue - (int)engineerSliderValue) >= 0) {
-            fighterLevelSlider.setMax(15 - (int)pilotSliderValue - (int)tradeSliderValue - (int)engineerSliderValue);
+        if (15 - ((int) pilotSliderValue - (int) tradeSliderValue - (int) engineerSliderValue) >= 0) {
+            fighterLevelSlider.setMax(15 - (int) pilotSliderValue - (int) tradeSliderValue - (int) engineerSliderValue);
         } else {
             fighterLevelSlider.setMax(0);
         }
-        if (15 - ((int)fighterSliderValue - (int)pilotSliderValue - (int)engineerSliderValue) >= 0) {
-            traderLevelSlider.setMax(15 - (int)fighterSliderValue - (int)pilotSliderValue - (int)engineerSliderValue);
+        if (15 - ((int) fighterSliderValue - (int) pilotSliderValue - (int) engineerSliderValue) >= 0) {
+            traderLevelSlider.setMax(15 - (int) fighterSliderValue - (int) pilotSliderValue - (int) engineerSliderValue);
         } else {
             traderLevelSlider.setMax(0);
         }
-        if (15 - ((int)fighterSliderValue - (int)tradeSliderValue - (int)pilotSliderValue) >= 0) {
-            engineerLevelSlider.setMax(15 - (int)fighterSliderValue - (int)tradeSliderValue - (int)pilotSliderValue);
+        if (15 - ((int) fighterSliderValue - (int) tradeSliderValue - (int) pilotSliderValue) >= 0) {
+            engineerLevelSlider.setMax(15 - (int) fighterSliderValue - (int) tradeSliderValue - (int) pilotSliderValue);
         } else {
             engineerLevelSlider.setMax(0);
         }
-        skillPointsAvailableLabel.setText((15 - (int)pilotSliderValue - (int)fighterSliderValue - (int)tradeSliderValue - (int)engineerSliderValue) + "/15");
-        skillPointsAvailable.setProgress(1 - (pilotSliderValue/15.0 + fighterSliderValue/15.0 + tradeSliderValue/15.0 + engineerSliderValue/15.0));
+        skillPointsAvailableLabel.setText((15 - (int) pilotSliderValue - (int) fighterSliderValue - (int) tradeSliderValue - (int) engineerSliderValue) + "/15");
+        skillPointsAvailable.setProgress(1 - (pilotSliderValue / 15.0 + fighterSliderValue / 15.0 + tradeSliderValue / 15.0 + engineerSliderValue / 15.0));
 //        if (pilotSliderValue <  pilotLevelSlider.getValue() || fighterSliderValue < fighterLevelSlider.getValue() || tradeSliderValue < traderLevelSlider.getValue() || engineerSliderValue < engineerLevelSlider.getValue()) {
 //            pilotLevelSlider.adjustValue(Math.floor(pilotSliderValue));
 //            fighterLevelSlider.adjustValue(Math.floor(fighterSliderValue));

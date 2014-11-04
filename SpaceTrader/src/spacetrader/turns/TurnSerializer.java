6707959ea92ba8e82b1e2dc6c1e9d3094182a17a
Gaby <Gaby@Gaby-PC>
2014-11-03 20:45:37 -0500

    Javadocked and fixed checkstyle errors in TurnSerializer.java

--- SpaceTrader/src/spacetrader/turns/TurnSerializer.java
+++ SpaceTrader/src/spacetrader/turns/TurnSerializer.java
@@ -4,27 +4,40 @@ import java.util.ArrayList;
 import java.io.Serializable;
 
 /**
- * This class may be used to save TurnEvent data
+ * This class may be used to save TurnEvent data.
  * @author Alex
  */
 public class TurnSerializer implements Serializable {
+    /**
+     * An ArrayList of listeners.
+     */
     private ArrayList<TurnListener> listeners;
+    /**
+     * The current turn the player is on.
+     */
     private int turn = 0;
     
-    TurnSerializer(ArrayList<TurnListener> listeners, int turn) {
-        this.listeners = listeners;
-        this.turn = turn;
+    /**
+     * Initializes the turn and the ArrayList of listeners.
+     * @param listeners2 ArrayList of listeners.
+     * @param turn2 The turn you are on
+     */
+    TurnSerializer(ArrayList<TurnListener> listeners2, int turn2) {
+        this.listeners = listeners2;
+        this.turn = turn2;
     }
     
     /**
-     * Get the listeners of the serialized object- package protected
+     * Get the listeners of the serialized object- package protected.
+     * @return listeners returns the ArrayList of listeners.
      */
     ArrayList<TurnListener> getListeners() {
         return listeners;
     }
     
     /**
-     * Get the turn number of the serialized object- package protected
+     * Get the turn number of the serialized object- package protected.
+     * @return turn the turn the player is currently on.
      */
     int getTurn() {
         return turn;
